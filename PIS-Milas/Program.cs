6e321712771133076e2b80a1b4ae6adaa5c4d6d1
student <Student@B420-01.fesb.hr>
2021-03-22 19:48:11 +0100

    Add project files.

---
+++ PIS-Milas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIS_Milas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
